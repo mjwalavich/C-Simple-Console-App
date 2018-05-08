e6793f1a458c372d8d6b35dece3abe8a6f75814c
Matthew Walavich <f55010571@leeschools.net>
2018-05-08 10:34:20 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FitnessTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int exerciseTime = 0;
+            bool exerciseMore = true;
+
+            while(exerciseMore)
+            {
+                Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
+
+                string entry = Console.ReadLine();
+
+                if (entry =="quit")
+                {
+                    exerciseMore = false;
+                }
+                else
+                {
+                    int minutes = int.Parse(entry);
+                    exerciseTime = exerciseTime + minutes;
+
+                    Console.WriteLine("You've entered " + exerciseTime + " minutes");
+                }
+            }
+            Console.WriteLine("Goodbye");
+        }
+    }
+}
