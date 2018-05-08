9d616266b42209e75f5a10f5bfab83bf87a50203
Matthew Walavich <f55010571@leeschools.net>
2018-05-08 13:43:19 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,42 +3,45 @@ using System;
 namespace FitnessTracker {
  class Program {
   static void Main(string[] args) {
-    int exerciseTime = 0;
-    bool exerciseMore = true;
+   double exerciseTime = 0;
+   bool exerciseMore = true;
 
-    while (exerciseMore) {
-     Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
+   while (exerciseMore) {
+    Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
 
-     string entry = Console.ReadLine();
+    string entry = Console.ReadLine();
 
-     if (entry == "quit") {
-      exerciseMore = false;
-     } else {
-      try {
-       int minutes = int.Parse(entry);
+    if (entry.ToLower() == "quit") {
+     exerciseMore = false;
+    } else {
+     try {
+      double minutes = double.Parse(entry);
 
-       if (minutes <= 0) {
-        Console.WriteLine("You cannot exercise for that amount of time.  Please enter another value.");
-        continue;
-       } else if (minutes <= 10) {
-        Console.WriteLine("It is a good start");
-       } else if (minutes <= 30) {
-        Console.WriteLine("Keep up the momentum");
-       } else if (minutes <= 60) {
-        Console.WriteLine("Getting back in shape, I see");
-       } else {
-        Console.WriteLine("Showoff");
-       }
-
-       exerciseTime = exerciseTime + minutes;
-      } catch (FormatException) {
-       Console.WriteLine("Invalid input. Try again.");
+      if (minutes <= 0) {
+       Console.WriteLine("You cannot exercise for that amount of time.  Please enter another value.");
        continue;
+      } else if (minutes <= 10) {
+       Console.WriteLine("It is a good start");
+      } else if (minutes <= 30) {
+       Console.WriteLine("Keep up the momentum");
+      } else if (minutes <= 60) {
+       Console.WriteLine("Getting back in shape, I see");
+      } else {
+       Console.WriteLine("Showoff");
       }
 
-
-
-      Console.WriteLine("You've entered " + exerciseTime + " minutes");
+      exerciseTime = exerciseTime + minutes;
+     } catch (FormatException) {
+      Console.WriteLine("Invalid input. Try again.");
+      continue;
      }
+
+
+
+     Console.WriteLine("You've entered " + exerciseTime + " minutes");
     }
-    Console.WriteLine("Goodbye");
+   }
+   Console.WriteLine("Goodbye");
+  }
+ }
+}
